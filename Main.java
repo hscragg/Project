18f574308f626b52f56412fd018fbda63aa35d46
hscragg <hayden.scragg@patelhs.org>
2020-10-19 17:11:41 +0000

    pushing out to regarde

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+   int myAge = 15;
+   int myBirthday = 0617;
+   int todaysDate = 0612;
+
+    if (myBirthday == todaysDate)
+    {
+      System.out.print("Happy Birthday!!");
+      System.out.println(" I am now " + (myAge + 1));  
+    }
+    else
+    {
+      System.out.print("It is not my birthday");
+      System.out.println(" and I am still " + myAge);
+    }
   }
 }
